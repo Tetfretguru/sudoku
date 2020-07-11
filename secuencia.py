6cc2a43539a7ce0ef902e25cafdaa3dd16004365
Martin Ghiena <josemartinghiena@gmail.com>
2020-07-11 17:41:11 -0300

    Se generan las listas para comenzar con las pruebas de sumatorias

---
+++ secuencia.py
@@ -0,0 +1,12 @@
+import math
+
+def completar(vertical, horizontal):
+    pass
+    
+
+
+if __name__ == '__main__':
+    horizontal = list(range(1, 10))
+    vertical = list(range(1, 10))
+
+    print(sum(horizontal))
